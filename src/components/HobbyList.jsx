35ba1a24da06892f8e555a530123c76b3da39b98
Nazarius9273827 <nazarbenevskij@gmail.com>
2025-04-03 19:38:43 +0300

    Add Hobby List

---
+++ src/components/HobbyList.jsx
@@ -0,0 +1,11 @@
+
+
+export const HobbyList = () => {
+    return <ul>
+        <li>Swimming</li>
+        <li>Drawing</li>
+        <li>Swimming</li>
+        <li>Swimming</li>
+        <li>Swimming</li>
+    </ul>
+}
